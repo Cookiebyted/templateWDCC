1c457c30691dc7f571d9fa1a59105eca89ab3060
Cookiebyted <69370714+Cookiebyted@users.noreply.github.com>
2020-08-08 17:57:44 +1200

    Completed the Titles

--- components/header.js
+++ components/header.js
@@ -2,41 +2,41 @@ import Link from 'next/link'
 
 const MenuItem = ({ children, href }) =>
   <Link href={href}>
-    <li className='block mt-4 inline-block  rounded-md text-center bg-orange-200 hover:bg-orange-500 px-4 py-2 m-2 lg:mt-0 hover:text-white mr-4'>
+    <li className='block mt-4 inline-block  rounded-md text-center bg-blue-200 hover:bg-blue-500 px-4 py-2 m-2 lg:mt-0 hover:text-white mr-4'>
       <a className='no-underline hover:text-white'>{children}</a>
     </li>
   </Link>
 
 function Header ({ user, loading }) {
+  console.log(JSON.stringify(user))
   return (
-    <header className='bg-orange-300'>
+    <header className='bg-white shadow-md'>
       <nav className='container flex items-center justify-between flex-wrap  py-2 '>
-        <div id='Brand' className='text-3xl text-black-700'>
-          First Timers Ltd.
-        </div>
-        <ul className=' flex   '>
+        {/* <div id='Brand' className='text-3xl text-black-700'> */}
+        {/* <div style={{width: 50, height: 50, margin: 'auto'}}> */}
+        <a style={{width: 100, height: 23, margin: 'auto'}} href='https://canvas.auckland.ac.nz/calendar#view_name=month&view_start=2020-08-05'>
+        <img src='Canvaslogo.png'/>
+        </a>
+        {/* </div> */}
+         {/* alskfjdlaskjfd
+        </div> */}
+        {/* <ul className=' flex   '>
           <MenuItem href='/'>
-              Home
+          Hi {user && user.nickname}!
           </MenuItem>
-          <MenuItem href='/about'>
-              About
-          </MenuItem>
+          <img src={user.picture} alt="User Avatar" width="45" height="20" />
+
           {!loading &&
             (user ? (
               <>
-                <MenuItem href='/reportSWR'>
-                Report
-                </MenuItem>
-                <MenuItem href='/api/logout'>
-                Logout
-                </MenuItem>
+            
               </>
             ) : (
               <MenuItem href='/api/login'>
                 Login
               </MenuItem>
             ))}
-        </ul>
+        </ul> */}
       </nav>
 
     </header>
--- pages/hello.js
+++ pages/hello.js
@@ -1,12 +1,29 @@
 import Layout from '../components/layout'
-import { Greet, Hello } from '../components/Hello'
+
 // React Components can be very very terse
-const name = 'Henry'
+const name = 'Matthew'
 export default () =>
   <Layout>
     <article>
-      <h1>React Hello World</h1>
-      <Greet />
-      <Hello name={name} />
+      <div style={{
+        marginTop: 30
+      }}>
+        <h1>COMPSCI335 - WK01L03</h1>
+      </div>
+      <div style={{
+        background: "black",
+        height: 200,
+        width: 400,
+        marginTop: 5
+      }} />
+      <div class="flex mb-4">
+        <div class="flex-1 h-12"><h2>About</h2></div>
+        <div class="flex-1 h-12"><h2>Piazza</h2></div>
+        <div class="flex-1 h-12"><h2>Lectures</h2></div>
+        <div class="flex-1 h-12"></div>
+        <div class="flex-1 h-12"></div>
+        <div class="flex-1 h-12"></div>
+        <div class="flex-1 h-12"></div>
+      </div>
     </article>
   </Layout>
